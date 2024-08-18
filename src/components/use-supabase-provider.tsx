4590488e4dad5cff29b5983ad98950c6d5d8edc8
Tri Nguyen Van <nguyenvantri2820@gmail.com>
2024-08-18 21:11:52 +0700

    feat

--- src/components/use-supabase-provider.tsx
+++ src/components/use-supabase-provider.tsx
@@ -4,10 +4,12 @@ import {
   noSerialize,
   NoSerialize,
   QRL,
+  Signal,
   Tracker,
   useContext,
   useContextProvider,
   useResource$,
+  useSignal,
   useStore,
   useVisibleTask$,
 } from "@builder.io/qwik";
@@ -61,3 +63,72 @@ export function useSupabaseResourceQrl<T>(
 }
 
 export const useSupabaseResource$ = implicit$FirstArg(useSupabaseResourceQrl);
+
+export function useSupabaseRealtime<T extends { id: string }>(_: {
+  filter: Signal<
+    | {
+        table: string;
+        col: string;
+        value: string;
+      }
+    | undefined
+  >;
+}) {
+  const supabase = useContext(SupabaseContext);
+  const loaded = useSignal<boolean>(false);
+  const latest_inserted = useSignal<T>();
+  const rows = useSignal<T[]>([]);
+  useVisibleTask$(async ({ track, cleanup }) => {
+    track(_.filter);
+    const client = track(() => supabase.client);
+    if (!client) return;
+    if (!_.filter.value) return;
+
+    const _select = await client
+      .from(_.filter.value.table)
+      .select()
+      .eq(_.filter.value.col, _.filter.value.value);
+
+    loaded.value = true;
+    if (_select.error) {
+      console.warn("error", _select.error);
+      return;
+    }
+
+    rows.value = _select.data as T[];
+    console.log("subscribe to", _.filter.value);
+    client
+      .channel("db-changes")
+      .on(
+        "postgres_changes",
+        {
+          event: "INSERT",
+          schema: "public",
+          table: _.filter.value.table,
+          filter: `${_.filter.value.col}=eq.${_.filter.value.value}`,
+        },
+        (payload) => {
+          console.log("received new for", _.filter.value);
+          const m = payload.new as T;
+          if (rows.value.some((n) => n.id == m.id)) return;
+          rows.value = [...rows.value, m];
+
+          latest_inserted.value = m;
+        },
+      )
+      // TODO: handle UPDATE, DELETE
+      .subscribe();
+
+    cleanup(() => {
+      rows.value = [];
+      loaded.value = false;
+      // it seems that the handle is automatically cleaned somehow?
+    });
+  });
+
+  return {
+    rows,
+    latest_inserted,
+    loaded,
+  };
+}
--- src/routes/channel/[id]/index.tsx
+++ src/routes/channel/[id]/index.tsx
@@ -6,6 +6,7 @@ import {
   useComputed$,
   useContext,
   useSignal,
+  useVisibleTask$,
 } from "@builder.io/qwik";
 import { useLocation, type DocumentHead } from "@builder.io/qwik-city";
 import { BsSendFill } from "@qwikest/icons/bootstrap";
@@ -17,12 +18,13 @@ import TopBar from "~/components/top-bar";
 import { DataContext } from "~/components/use-data-provider";
 import {
   SupabaseContext,
+  useSupabaseRealtime,
   useSupabaseResource$,
   useSupabaseResourceQrl,
 } from "~/components/use-supabase-provider";
 import { dateF } from "~/utils";
 
-const MessagePending = component$(() => {
+const MessageLoading = component$(() => {
   return (
     <div class="animate-pulse">
       <div class="flex cursor-pointer items-center space-x-4 px-8 py-4 hover:bg-neutral-100 hover:dark:bg-neutral-800">
@@ -67,86 +69,104 @@ const MessagePending = component$(() => {
 
 const TextChannelView = component$(
   (_: { channel: Signal<Channel | undefined> }) => {
-    const messageResource = useSupabaseResource$(
-      async ({ track, cleanup, client }) => {
-        track(_.channel);
-
-        const controller = new AbortController();
-        cleanup(() => {
-          controller.abort();
-        });
-
-        if (_.channel.value?.type !== "text") return;
-
-        return await client
-          .from("message")
-          .select()
-          .eq("thread_id", _.channel.value.id)
-          .abortSignal(controller.signal);
-      },
+    const filter = useComputed$(() =>
+      _.channel.value?.id
+        ? {
+            table: "message",
+            col: "thread_id",
+            value: _.channel.value.id,
+          }
+        : undefined,
     );
-    const text = useSignal("");
-    const submit = $(() => {});
+    const ref = useSignal<HTMLElement>();
+    const {
+      rows: messages,
+      loaded,
+      latest_inserted,
+    } = useSupabaseRealtime<{
+      id: string;
+      content: string;
+      created_at: string;
+    }>({
+      filter,
+    });
+
+    const supabase = useContext(SupabaseContext);
+    const input = useSignal("");
+    const submit = $(async () => {
+      if (!input.value) return;
+      const _insert = await supabase.client!.from("message").insert({
+        thread_id: _.channel.value!.id,
+        content: input.value,
+      });
+      input.value = "";
+    });
+
+    useVisibleTask$(({ track }) => {
+      track(latest_inserted);
+      // TODO: Only if the latest_inserted's author is me, do I scroll
+      // Else, show number of new messages and show hint to scroll
+      ref.value!.scrollTo({
+        top: ref.value!.scrollHeight,
+        behavior: "smooth",
+      });
+    });
+
+    useVisibleTask$(({ track }) => {
+      track(loaded);
+      track;
+      ref.value!.scrollTo({
+        top: ref.value!.scrollHeight,
+        behavior: "instant",
+      });
+    });
+
     return (
       <>
-        <div class="flex-1 overflow-y-auto pb-4">
-          <Resource
-            value={messageResource}
-            onPending={() => <MessagePending />}
-            onRejected={(reason) => (
-              <div class="px-8 py-4 text-sm text-neutral-500">
-                Error: {reason.message}
-              </div>
-            )}
-            onResolved={(res) => {
-              if (!res) return <div></div>;
-              if (res.error)
-                return (
-                  <div class="px-8 py-4 text-sm text-neutral-500">
-                    Error: {res.error.message}
-                  </div>
-                );
-              if (res.data.length == 0)
-                return (
-                  <div class="px-8 py-4 text-sm text-neutral-500">
-                    No messages
-                  </div>
-                );
-              return (
-                <div>
-                  {res.data.map((message) => (
-                    <div
-                      key={message.id}
-                      class="flex cursor-pointer items-center space-x-4 px-8 py-4 hover:bg-neutral-100 hover:dark:bg-neutral-800"
-                    >
-                      <img
-                        src={ExampleAvatar}
-                        class="h-12 w-12 flex-none rounded-lg border"
-                      />
-                      <div class="flex-1 space-y-1">
-                        <div class="font-semibold">
-                          Tri
-                          <span class="ml-2 text-xs font-normal text-neutral-500 dark:text-neutral-400">
-                            {dateF(message.created_at)}
-                          </span>
-                        </div>
-
-                        <div class="">{message.content}</div>
+        <div class="flex-1 overflow-y-auto pb-4" ref={ref}>
+          {loaded.value ? (
+            messages.value.length == 0 ? (
+              <div class="px-8 py-4 text-sm text-neutral-500">No messages</div>
+            ) : (
+              <div>
+                {messages.value.map((message) => (
+                  <div
+                    key={message.id}
+                    class="flex cursor-pointer items-center space-x-4 px-8 py-4 hover:bg-neutral-100 hover:dark:bg-neutral-800"
+                  >
+                    <img
+                      src={ExampleAvatar}
+                      class="h-12 w-12 flex-none rounded-lg border"
+                    />
+                    <div class="flex-1 space-y-1">
+                      <div class="font-semibold">
+                        Tri
+                        <span class="ml-2 text-xs font-normal text-neutral-500 dark:text-neutral-400">
+                          {dateF(message.created_at)}
+                        </span>
                       </div>
+
+                      <div class="">{message.content}</div>
                     </div>
-                  ))}
-                </div>
-              );
-            }}
-          />
+                  </div>
+                ))}
+              </div>
+            )
+          ) : (
+            <MessageLoading />
+          )}
         </div>
 
         <div class="flex-none space-y-2 px-4 pb-4">
           <div class=" flex  items-start rounded-lg border  bg-neutral-100 dark:bg-neutral-800 ">
             <textarea
-              bind:value={text}
+              bind:value={input}
               onKeyDown$={(ev: KeyboardEvent) => {
-                if (ev.ctrlKey && ev.key === "Enter") {
+                if (
+                  // ev.ctrlKey &&
+                  ev.key === "Enter"
+                ) {
+                  ev.preventDefault();
                   submit();
                 }
               }}
@@ -160,7 +180,7 @@ const TextChannelView = component$(
             </button>
           </div>
 
-          <div class="text-xs text-neutral-500">Ctrl + Enter to send</div>
+          {/* <div class="text-xs text-neutral-500">Ctrl + Enter to send</div> */}
         </div>
       </>
     );
